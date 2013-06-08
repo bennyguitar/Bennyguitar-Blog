05113886e07c01e6dd83eae77b26de900671e5d2
BenGordon <brgordon@ua.edu>
2013-06-08 15:42:21 -0500

    Initial blog commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bennyguitarblog::Application.config.secret_token = 'b34c0dcbd8e2e1721a63970aba3fde90307b3f8607dcb4713e907330687f1dd5e37b6fc0c7423dee45d7453dcee89931307242e1bb37478f87b9df22d821aa1d'
